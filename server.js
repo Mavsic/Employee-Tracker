7afc97d55a54ddd5c9f6340bb83b8e3ba79a2f0a
Mavsic <muminova.m@hotmail.com>
2022-11-02 13:06:17 -0400

    function to view employees department etc

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const connection = mySQL2.createConnection ({
     password: 'Khursheda777!'
      });
 
-     unction promptUser () {
+     function promptUser () {
         inquirer
         .prompt([
             {
@@ -94,3 +94,79 @@ const connection = mySQL2.createConnection ({
               // }
         });
       };
+
+
+      // View All Employees
+const viewAllEmployees = () => {
+    let sql =       `SELECT employee.id, 
+                    employee.first_name, 
+                    employee.last_name, 
+                    role.title, 
+                    department.department_name AS 'department', 
+                    role.salary
+                    FROM employee, role, department 
+                    WHERE department.id = role.department_id 
+                    AND role.id = employee.role_id
+                    ORDER BY employee.id ASC`;
+    connection.promise().query(sql)
+     .then(([rows]) => {
+      console.table(rows)
+      promptUser();
+     })
+  };
+
+// View all Roles
+const viewAllRoles = () => {
+  const sql =     `SELECT role.id, role.title, department.department_name AS department
+                  FROM role
+                  INNER JOIN department ON role.department_id = department.id`;
+  connection.promise().query(sql)
+  .then(([rows]) => {
+  console.table(rows)
+  promptUser();
+  });
+};
+
+// View all Employees by Department
+const viewEmployeesByDepartment = () => {
+    const sql =     `SELECT employee.first_name, 
+                    employee.last_name, 
+                    department.department_name AS department
+                    FROM employee 
+                    LEFT JOIN role ON employee.role_id = role.id 
+                    LEFT JOIN department ON role.department_id = department.id`;
+    connection.promise().query(sql)
+      .then(([rows]) => {
+      console.table(rows)
+      promptUser();
+      });
+  };
+  
+  // View all Departments
+  const viewAllDepartments = () => {
+    const sql =   `SELECT department.id AS id, department.department_name AS department FROM department`; 
+    connection.promise().query(sql)
+     .then(([rows]) => {
+      console.table(rows)
+      promptUser();
+    });
+  };
+
+ //View all Departments by Budget
+  const viewDepartmentBudget = () => {
+   const sql =     `SELECT department_id AS id, 
+                    department.department_name AS department,
+                    SUM(salary) AS budget
+                    FROM  role 
+                    INNER JOIN department ON role.department_id = department.id GROUP BY  role.department_id`;
+    connection.promise().query(sql) 
+    .then(([rows]) => {
+    console.table(rows)
+    promptUser();
+    });
+  };
+
+  
+  
+
+  promptUser();
